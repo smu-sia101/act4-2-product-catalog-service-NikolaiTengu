90c780fdcf85fcb32a98a2f6354cb87b7c220b00
MARLOWE IAN JUMAGBAS <hed-mijumagbas@smu.edu.ph>
2025-03-28 18:47:25 +0800

    all

---
+++ ProductCatalogService/Controllers/ProductConnectServerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalogService.Controllers
+{
+    [ApiController]
+    [Route("[Controller")]
+    public class ProductConnectServerController : Controller
+    {
+        [HttpGet("/api/products")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/api/products/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("/api/products")]
+        public IActionResult AddProduct()
+        {
+            return Ok();
+        }
+
+        [HttpPut("/api/products/{id}")]
+        public IActionResult UpdateProduct(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("/api/products/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            return Ok();
+        }
+    }
+
+}
---
+++ ProductCatalogService/product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ProductCatalogService
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Stock { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
